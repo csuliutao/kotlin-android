7008a77e01816cf594015c9d566c7b4446e9f4f7
liutao <csuliutao@sina.cn>
2019-04-21 09:47:09 +0800

    密封类使用，有限的子类类型；枚举类，有限的对象个数；

--- app/src/main/java/com/csu/liutao/kotlin/layouts/MainUI.kt
+++ app/src/main/java/com/csu/liutao/kotlin/layouts/MainUI.kt
@@ -6,17 +6,27 @@ import android.view.View
 import android.widget.LinearLayout
 import com.csu.liutao.cotlin.R
 import com.csu.liutao.kviews.circleImageView
+import com.csu.liutao.kviews.drawables.CircleDrawable
+import com.csu.liutao.kviews.drawables.CircleImageClipDrawable
+import com.csu.liutao.kviews.drawables.CircleImageLayerDrawable
 import com.csu.liutao.kviews.pieView
 import org.jetbrains.anko.*
 
 class MainUI : AnkoComponent<Activity> {
     override fun createView(ui: AnkoContext<Activity>): View = with(ui) {
         verticalLayout {
+            orientation = LinearLayout.VERTICAL
             pieView {
                 padding = dip(20)
-                pieDatas = mutableMapOf(0.3F to Color.RED, 0.2F to Color.BLUE, 0.5F to Color.GREEN)
+                pieDatas = mutableMapOf(0.3F to Color.RED, 0.2F to Color.BLUE, 0.4F to Color.GREEN, 0.1F to Color.CYAN)
                 hasClickAnimator = true
                 isCirclePie = true
+            }.lparams(width = dip(300), height = dip(350))
+
+            circleImageView {
+                imageResource = R.mipmap.circle
+                padding = dip(20)
+                circleDrawable = CircleImageLayerDrawable()
             }.lparams(width = dip(300), height = dip(350))
         }
     }
--- kviews/src/main/java/com/csu/liutao/kviews/drawables/CircleDrawable.kt
+++ kviews/src/main/java/com/csu/liutao/kviews/drawables/CircleDrawable.kt
@@ -1,11 +1,10 @@
 package com.csu.liutao.kviews.drawables
 
-import android.graphics.Canvas
-import android.graphics.Rect
+import android.graphics.*
 import android.graphics.drawable.Drawable
 import kotlin.math.min
 
-abstract class CircleDrawable{
+sealed class CircleDrawable{
     protected var radius = 0F
     protected var centerX = 0F
     protected var centerY = 0F
@@ -22,4 +21,80 @@ abstract class CircleDrawable{
         set(value) { field = value}
 
     abstract fun draw(canvas: Canvas)
+}
+
+class CircleImageClipDrawable: CircleDrawable() {
+    override fun draw(canvas: Canvas) {
+        if (drawable == null) return
+        canvas.save()
+        val path = Path()
+        path.addCircle(centerX, centerY, radius, Path.Direction.CW)
+        canvas.clipPath(path)
+
+        val temPaint = Paint()
+        temPaint.isAntiAlias = true
+        // 图片需显示部分左上角移动到裁剪区域左上角，不缩放图片
+        val srcBmp = Bitmap.createBitmap(drawable!!.intrinsicWidth, drawable!!.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val srcCanvas = Canvas(srcBmp)
+        drawable!!.draw(srcCanvas)
+
+        val tempMatrix = Matrix()
+        tempMatrix.postTranslate(centerX - srcBmp.width / 2, centerY - srcBmp.height / 2)
+        val tempScale = radius * 2 / Math.min(srcBmp.width, srcBmp.height)
+        tempMatrix.postScale(tempScale, tempScale, centerX, centerY)
+        canvas.drawBitmap(srcBmp, tempMatrix, temPaint)
+        canvas.restore()
+    }
+}
+
+class CircleImageLayerDrawable: CircleDrawable(){
+    override fun draw(canvas: Canvas) {
+        if (drawable == null) return
+        val paint = Paint()
+        paint.isAntiAlias = true
+        val layerId =
+            canvas!!.saveLayer(centerX - radius, centerY - radius, centerX + radius, centerY + radius, paint)
+        // 绘制圆形图层
+        val dstBmp = Bitmap.createBitmap((centerX + radius).toInt(), (centerY + radius).toInt(), Bitmap.Config.ARGB_8888)
+        val dstCanvas = Canvas(dstBmp)
+        dstCanvas.drawCircle(centerX, centerY, radius, paint)
+        canvas.drawBitmap(dstBmp, 0F, 0F, paint)
+
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+
+        // 图片需显示部分左上角移动到裁剪区域左上角，不缩放图片
+        val srcBmp = Bitmap.createBitmap(drawable!!.intrinsicWidth, drawable!!.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val srcCanvas = Canvas(srcBmp)
+        drawable!!.draw(srcCanvas)
+
+        val temMatrix = Matrix()
+        temMatrix.postTranslate(centerX - drawable!!.intrinsicWidth / 2, centerY - drawable!!.intrinsicHeight / 2)
+        val tempScale = radius * 2 / Math.min(srcBmp.width, srcBmp.height)
+        temMatrix.postScale(tempScale, tempScale, centerX, centerY)
+        canvas.drawBitmap(srcBmp, temMatrix, paint)
+
+        paint.xfermode = null
+        canvas.restoreToCount(layerId)
+    }
+}
+
+class CircleImageShaderDrawable: CircleDrawable() {
+    override fun draw(canvas: Canvas) {
+        if (drawable == null) return
+        val srcBmp = Bitmap.createBitmap(drawable!!.intrinsicWidth, drawable!!.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val srcCanvas = Canvas(srcBmp)
+        drawable!!.draw(srcCanvas)
+
+        val tempMatrix = Matrix()
+        tempMatrix.postTranslate(centerX - srcBmp.width / 2, centerY - srcBmp.height / 2)
+        val tempScale = radius * 2 / Math.min(srcBmp.width, srcBmp.height)
+        tempMatrix.postScale(tempScale, tempScale, centerX, centerY)
+
+        val paint = Paint()
+        paint.isAntiAlias = true
+        paint.shader = BitmapShader(srcBmp, Shader.TileMode.MIRROR, Shader.TileMode.MIRROR)
+        paint.shader.setLocalMatrix(tempMatrix)
+
+        canvas!!.drawCircle(centerX, centerY, radius, paint)
+    }
 }
